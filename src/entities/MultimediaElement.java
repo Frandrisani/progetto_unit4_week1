2afaf84c9104f3e82b444d010c5a311510d09c32
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-03-08 11:12:17 +0100

    Change to MultimediaElement class: I had made public void class instead of abstract class

--- src/entities/MultimediaElement.java
+++ src/entities/MultimediaElement.java
@@ -1,6 +1,9 @@
 package entities;
 
-public class MultimediaElement {
+//Questa classe è una classe astratta, ovvero non può essere istanziata direttamente, ma solo da altre classi.
+//Cos' facendo neghiamo la possibilità di istanziare direttamente un oggetto di tipo MultimediaElement
+//Questa classe ci servirà per estendere le altre classi figlie che avranno in cumune almeno il titolo.
+abstract class MultimediaElement {
 
     //Attributi
     //Perchè ho inserito "protected" invece di "private"? --> Perchè così il titolo è privato e non è accessibile dall'esterno,
